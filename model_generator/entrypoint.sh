bdbe0b0d572651c78dcb3de248cdff180db7b475
vinciusb <65973642+vinciusb@users.noreply.github.com>
2024-12-14 17:42:20 -0300

    a

--- model_generator/entrypoint.sh
+++ model_generator/entrypoint.sh
@@ -4,7 +4,7 @@ cd /tmp
 mkdir -p ./data
 rm /tmp/data/*
 wget -P /tmp/data $REPO_URL
-LATEST_TAG=$(wget -qO- https://api.github.com/repos/vinciusb/TP2-CN-DEPLOY/tags | jq 'sort_by(.name) | .[-1] | .name')
+LATEST_TAG=$(wget -qO- https://api.github.com/repos/vinciusb/TP2-CN-DEPLOY/tags | jq -r 'sort_by(.name) | .[-1] | .name')
 export LATEST_TAG
 cd /tmp
 python3 model_generator.py
